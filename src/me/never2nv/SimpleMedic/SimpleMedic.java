c8a9a49b16c6f3062bbe3be2b1a06fbd6376a8eb
Mathias <Matho97@gmail.com>
2013-06-05 20:01:47 +0200

    Fixed many errors

--- src/me/never2nv/SimpleMedic/SimpleMedic.java
+++ src/me/never2nv/SimpleMedic/SimpleMedic.java
@@ -1,6 +1,8 @@
 package me.never2nv.SimpleMedic;
 
 import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -35,109 +37,104 @@ public class SimpleMedic extends JavaPlugin{
                 getConfig().options().copyDefaults(true);
 		saveConfig();
 		
-	}
-    	}
-    
-    private boolean setupEconomy() {
+        }
+    }
+	
+	public static boolean setupEconomy() {
+	    Economy econ;
         //The actual code to search for an economy to use
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+        if (Bukkit.getServer().getPluginManager().getPlugin("Vault") == null) {
             return false;
         }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
         if (rsp == null) {
             return false;
         }
         econ = rsp.getProvider();
         return econ != null;
-}
+    }
 
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		boolean econenabled = plugin.getConfig().getBoolean("econ-enabled");
 		int cost = plugin.getConfig().getInt("econ-cost");
 		Player player = (Player) sender;
+		
 		if(commandLabel.equalsIgnoreCase("extme") || commandLabel.equalsIgnoreCase("extinguishme")){
 			if(args.length == 0){
-				if(player.hasPermission("simplemedic.ext")|| player.hasPermission("simplemedic.*") {
-				player.sendMessage(ChatColor.DARK_RED + "[MEDIC] You have been extinguished!");	
-				player.setFireTicks(0);
-			}else if(args.length == 1){
-				if(player.hasPermission("simplemedic.ext.others")|| player.hasPermission("simplemedic.*") {
-		                Player target = player.getServer().getPlayer(args[0]); 
-				target.setFireTicks(0);
-				player.sendMessage(ChatColor.DARK_RED + "[MEDIC] " + target.getName() + " has been extinguished!");
-			        target.sendMessage(ChatColor.DARK_RED + "[MEDIC] You have been extinguished!");
-				}else {
-				player.sendMessage(ChatColor.DARK_RED + "[MEDIC] Error: Incorrect usage.");
+				if(player.hasPermission("simplemedic.ext")|| player.hasPermission("simplemedic.*")) {
+					player.sendMessage(ChatColor.DARK_RED + "[MEDIC] You have been extinguished!");	
+					player.setFireTicks(0);
+				} else if(args.length == 1){
+					if(player.hasPermission("simplemedic.ext.others")|| player.hasPermission("simplemedic.*")) {
+				        Player target = player.getServer().getPlayer(args[0]); 
+				        target.setFireTicks(0);
+						player.sendMessage(ChatColor.DARK_RED + "[MEDIC] " + target.getName() + " has been extinguished!");
+				        target.sendMessage(ChatColor.DARK_RED + "[MEDIC] You have been extinguished!");
+					} else {
+						player.sendMessage(ChatColor.DARK_RED + "[MEDIC] Error: Incorrect usage.");
+					}
+				}
 			}
 		}
 		if(commandLabel.equalsIgnoreCase("medic") || commandLabel.equalsIgnoreCase("med")){
+            Player targetPlayer = player.getServer().getPlayer(args[0]);
 
 			
 			if(args.length == 0){
-				if(player.hasPermission("simplemedic.heal")|| player.hasPermission("simplemedic.*") {
-				if(econenabled) {
-                EconomyResponse r = econ.withdrawPlayer(player.getName(), cost);
-                if(r.transactionSuccess()) {
-                    //If the transaction succeeds it heals them else it tells them the error
-                    //healing player
-                    player.setHealth(20);
-                    player.setFireTicks(0);
-                    player.chat("/me " + ChatColor.DARK_RED + "just got healed by a " + ChatColor.GREEN + "Medic!");
-                    player.sendMessage(ChatColor.GREEN + "[MEDIC] " + ChatColor.WHITE + "You've Been Healed!");
-                    
-                } 
-                
-                else {
-                    player.sendMessage(String.format("An error occured: %s", r.errorMessage));
+				if(player.hasPermission("simplemedic.heal")|| player.hasPermission("simplemedic.*")) {
+					if(econenabled) {
+	                EconomyResponse r = econ.withdrawPlayer(player.getName(), cost);
+		                if(r.transactionSuccess()) {
+		                    //If the transaction succeeds it heals them else it tells them the error
+		                    //healing player
+		                    player.setHealth(20);
+		                    player.setFireTicks(0);
+		                    player.chat("/me " + ChatColor.DARK_RED + "just got healed by a " + ChatColor.GREEN + "Medic!");
+		                    player.sendMessage(ChatColor.GREEN + "[MEDIC] " + ChatColor.WHITE + "You've Been Healed!");
+		                    
+		                } else {
+		                    player.sendMessage(String.format("An error occured: %s", r.errorMessage));
+		                }
+					} else {
+			            player.setHealth(20);
+		                player.setFireTicks(0);
+		                player.chat("/me " + ChatColor.DARK_RED + "just got healed by a " + ChatColor.GREEN + "Medic!");
+		                player.sendMessage(ChatColor.GREEN + "[MEDIC] " + ChatColor.WHITE + "You've Been Healed!");	
+					}
+				}
+			} else if(args.length == 1){
+                if(player.hasPermission("simplemedic.heal.others") || player.hasPermission("simplemedic.*") ) {
+	               	if(econenabled) {
+	               		EconomyResponse r = econ.withdrawPlayer(player.getName(), cost);
+			            if(r.transactionSuccess()) {
+			            	//See above :D
+			                if(player.getServer().getPlayer(args[0]) !=null){
+			                	targetPlayer.setHealth(20);
+			                    targetPlayer.setFireTicks(0);
+			                    targetPlayer.getBedSpawnLocation();
+			                    player.chat("/me " + ChatColor.DARK_RED + "just got healed by a " + ChatColor.GREEN + "Medic!");
+			                    player.sendMessage(ChatColor.GREEN + "[MEDIC] " + ChatColor.WHITE + "You've Been Healed!");
+			                    return true;
+			                } else {
+			                	// checks to see if player is online, else displays this message!
+			                    player.sendMessage(ChatColor.RED + "PLAYER IS NOT ONLINE!");
+			                    return true;
+			                } 
+			            }  else {
+			              	player.sendMessage(String.format("An error occured: %s", r.errorMessage));
+			            }
+			        } else {
+			          	targetPlayer.setHealth(20);
+			            targetPlayer.setFireTicks(0);
+			            targetPlayer.getBedSpawnLocation();
+			            player.chat("/me " + ChatColor.DARK_RED + "just got healed by a " + ChatColor.GREEN + "Medic!");
+			            player.sendMessage(ChatColor.GREEN + "[MEDIC] " + ChatColor.WHITE + "You've Been Healed!");	
+			        }
                 }
-			} else {
-	            player.setHealth(20);
-                    player.setFireTicks(0);
-                    player.chat("/me " + ChatColor.DARK_RED + "just got healed by a " + ChatColor.GREEN + "Medic!");
-                    player.sendMessage(ChatColor.GREEN + "[MEDIC] " + ChatColor.WHITE + "You've Been Healed!");	
-			}
-			}
-			}
-                else if(args.length == 1){
-                	if(player.hasPermission("simplemedic.heal.others") || player.hasPermission("simplemedic.*") ) {
-                	if(econenabled) {
-                EconomyResponse r = econ.withdrawPlayer(player.getName(), cost);
-                if(r.transactionSuccess()) {
-                    //See above :D
-                    if(player.getServer().getPlayer(args[0]) !=null){
-                        Player targetPlayer = player.getServer().getPlayer(args[0]);
-                        targetPlayer.setHealth(20);
-                        targetPlayer.setFireTicks(0);
-                        targetPlayer.getBedSpawnLocation();
-                        player.chat("/me " + ChatColor.DARK_RED + "just got healed by a " + ChatColor.GREEN + "Medic!");
-                        player.sendMessage(ChatColor.GREEN + "[MEDIC] " + ChatColor.WHITE + "You've Been Healed!");
-                    }
-                    
-                    else {
-                        // checks to see if player is online, else displays this message!
-                        player.sendMessage(ChatColor.RED + "PLAYER IS NOT ONLINE!");
-                         } 
-                }
-                    
-                    else {
-                    player.sendMessage(String.format("An error occured: %s", r.errorMessage));
-                         } else {
-                        targetPlayer.setHealth(20);
-                        targetPlayer.setFireTicks(0);
-                        targetPlayer.getBedSpawnLocation();
-                        player.chat("/me " + ChatColor.DARK_RED + "just got healed by a " + ChatColor.GREEN + "Medic!");
-                        player.sendMessage(ChatColor.GREEN + "[MEDIC] " + ChatColor.WHITE + "You've Been Healed!");	
-                         }
-                	}
-                	}
-                    
-				
-		return false;
-		}
+            }
 			return false;
-		
-	}
+		}
 		return false;
 	}
 }
